4ccb0d37ccc5d8fd8976439c012caf33240afc16
Dimitrios Athanasakos <athanasakosdi@gmail.com>
2024-12-09 09:25:14 -0800

    added train and utils

--- Models/Diffusion.py
+++ Models/Diffusion.py
@@ -19,17 +19,17 @@ def GaussianFourierProjection(num_embed, scale=16.0, device='cpu'):
     return freq  # [half_dim]
 
 
-def Embedding(inputs, projection, num_embed=64, activation=F.silu, device='cpu'):
+def base_Embedding(inputs, projection):
     angle = inputs * projection.unsqueeze(0) * 1000.0  # broadcasting
     sin_angle = torch.sin(angle)        # Compute sine
     cos_angle = torch.cos(angle)        # Compute cosine
     embedding = torch.cat([sin_angle, cos_angle], dim=-1)  # Concatenate along the last dimension
 
-    dense1 = nn.Linear(2*num_embed, 2*num_embed) # since we are concatenating sin and cos along the last dimension
-    dense2 = nn.Linear(2*num_embed, num_embed)
+    #dense1 = nn.Linear(2*num_embed, 2*num_embed) # since we are concatenating sin and cos along the last dimension
+    #dense2 = nn.Linear(2*num_embed, num_embed)
     
-    embedding = activation(dense1(embedding))
-    embedding = activation(dense2(embedding))
+    #embedding = activation(dense1(embedding))
+    #embedding = activation(dense2(embedding))
 
     return embedding
 
@@ -68,7 +68,7 @@ def FF(features, min_proj=4, max_proj=8, device='cpu'):
 
     # sinusoidal expansions
     h = torch.cat([torch.sin(angle), torch.cos(angle)], dim=-1)  # [B, 2*D*num_freq]
-    return torch.cat([features, h], dim=-1)
+    return torch.cat([features, h], dim=-1) # [B, D+2*D*num_freq]
 
 
 def logsnr_schedule_cosine(t, logsnr_min=-20., logsnr_max=20.):
@@ -100,37 +100,126 @@ def get_logsnr_alpha_sigma(time, shape=(-1,1,1)):
 
 
 
-class ModelJet(nn.Module):
-    def __init__(self, num_jet, num_cond, num_embed, activation=F.silu):
-        super().__init__()
-        self.num_jet = num_jet
-        self.num_cond = num_cond
-        self.num_embed = num_embed
-        self.activation = activation
-
-        self.embed = GaussianFourierProjection(num_embed)
-        self.dense1 = nn.Linear(2*num_jet, 2*num_jet)
-        self.dense2 = nn.Linear(2*num_jet, num_jet)
-
-
 
 class GSGM(nn.Module):
-    def __init__(self, npart=100, config=None):
+    def __init__(self, npart=100, config=None, device='cpu'):
         super().__init__()
         if config is None:
             raise ValueError("Config file not given")
         
         self.config = config
-        self.num_feat = config['NUM_FEAT']
-        self.num_jet = config['NUM_JET']
-        self.num_cond = config['NUM_COND']
-        self.num_embed = config['EMBED']
+        self.activation = F.silu()
+        self.num_feat = self.config['NUM_FEAT']
+        self.num_jet = self.config['NUM_JET']
+        self.num_cond = self.config['NUM_COND']
+        self.num_embed = self.config['EMBED']
         self.max_part = npart
-        self.num_steps = config['MAX_STEPS']
-        self.activation = F.silu
+        self.num_steps = self.config['MAX_STEPS']
+        self.ema = 0.999
+        self.device = device
 
-        self.model_jet = ModelJet(self.num_jet, self.num_cond, self.num_embed, self.activation)
-        self.model_part = ModelPart(self.num_feat, self.num_jet, self.num_cond, self.num_embed, self.activation)
+        # parameters for FF(). We need to know the number of features before the forward pass
+        self.min_proj = 4
+        self.max_proj = 8
 
+        # Projection for time embedding
+        self.projection = GaussianFourierProjection(self.num_embed, scale=16.0, device=self.device)
+
+        # Embedding linear layers (instead of creating them in the Embedding function)
+        self.emb_fc1 = nn.Linear(2*self.num_embed, 2*self.num_embed)
+        self.emb_fc2 = nn.Linear(2*self.num_embed, self.num_embed)
+
+        # Layers equivalent to Keras Dense layers for conditional jet inputs
+        self.jet_fc1 = nn.Linear(self.num_jet, 2*self.num_embed)
+        self.jet_fc2 = nn.Linear(2*self.num_embed, self.num_embed)
+
+        # FF layers for cond
+        # We must know shape after FF; 
+        cond_ff_dim = self.num_cond * (1 + 2*(self.max_proj-self.min_proj))
+
+        self.cond_fc1 = nn.Linear(cond_ff_dim, 2*self.num_embed)
+        self.cond_fc2 = nn.Linear(2*self.num_embed, self.num_embed)
+
+        # graph_conditional and jet_conditional
+        self.graph_fc = nn.Linear(3*self.num_embed, 3*self.num_embed) # this should probably be 2*self.num_embed + self.num_embed//2 due to GaussianFourierProjection
+        self.jet_cond_fc = nn.Linear(2*self.num_embed, 2*self.num_embed) # similarly here, self.num_embed + self.num_embed//2
+  
+        # DeepSetsAtt
+        self.model_part_att = DeepSetsAtt(
+            num_feat=self.num_feat,
+            time_embedding_dim=3*self.num_embed,
+            num_heads=2,
+            num_transformer=6,
+            projection_dim=128,
+            use_dist=False
+        )
+
+        self.model_jet_att = DeepSetsAtt(
+            num_feat=self.num_jet,
+            time_embedding_dim=2*self.num_embed,
+            num_heads=2,
+            num_transformer=6,
+            projection_dim=128,
+            use_dist=False
+        )
+
+        self.ema_jet = None
+        self.ema_part = None
+
+
+
+    def embed_time(self, t):
+        # t: [B,1]
+        emb = base_Embedding(t, self.projection) # [B, 2*num_embed] (since sin+cos)
+        emb = self.activation(self.emb_fc1(emb))
+        emb = self.activation(self.emb_fc2(emb)) # [B,num_embed]
+        return emb
+
+
+
+    def forward_conditional_embeddings(self, t, cond, jet):
+        # t: [B,1], cond: [B,num_cond], jet: [B,num_jet]
+        t_emb = self.embed_time(t)  # [B,num_embed]
+
+        jet_dense = self.jet_fc1(jet)
+        #jet_dense = self.activation(jet_dense)              # In the original code, this is not activated. Unintented ? 
+        jet_dense = self.activation(self.jet_fc2(jet_dense)) # [B,num_embed]
+
+        cond_ff = FF(cond) # [B, cond_ff_dim], we assumed cond_ff_dim = num_cond*9
+        cond_dense = self.cond_fc1(cond_ff)
+        #cond_dense = self.activation(cond_dense)              # In the original code, this is not activated. Unintented ?
+        cond_dense = self.activation(self.cond_fc2(cond_dense)) # [B,num_embed]
+
+        graph_concat = torch.cat([t_emb, jet_dense, cond_dense], dim=-1) # [B,3*num_embed]
+        graph_conditional = self.activation(self.graph_fc(graph_concat)) # [B,3*num_embed]
+
+        jet_concat = torch.cat([t_emb, cond_dense], dim=-1) # [B,2*num_embed]
+        jet_conditional = self.activation(self.jet_cond_fc(jet_concat)) # [B,2*num_embed]
+
+        return graph_conditional, jet_conditional
+
+
+
+    def forward_part(self, part, t, jet, cond, mask):
+        graph_conditional, jet_conditional = self.forward_conditional_embeddings(t, cond, jet)
+        part_out = self.model_part_att(part, graph_conditional, mask=mask) # [B,N,num_feat]
+        return part_out
+
+
+
+    def forward_jet(self, jet, t, cond): 
+        graph_conditional, jet_conditional = self.forward_conditional_embeddings(t, cond, jet)
+        # jet: [B,num_jet] -> [B,1,num_jet]
+        jet_inp = jet.unsqueeze(1)
+        jet_out = self.model_jet_att(jet_inp, jet_conditional, mask=None) # [B,1,num_jet]
+        jet_out = jet_out.squeeze(1) # [B,num_jet]
+        return jet_out
+
+
+
+    def forward(self, part, jet, cond, mask, t):
+        part_pred = self.forward_part(part, t, jet, cond, mask)
+        jet_pred = self.forward_jet(jet, t, cond)
+        return jet_pred, part_pred
 
 
---
+++ train.py
@@ -0,0 +1,93 @@
+import numpy as np
+import yaml
+import os
+from Models.Diffusion import GSGM
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse
+import time
+import utils
+
+
+torch.random.set_seed(1233)
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()    
+    parser.add_argument('--config', default='config.yaml', help='Config file with training parameters')
+    parser.add_argument('--file_name', default='processed_data_background_rel.h5', help='File to load')
+    parser.add_argument('--npart', default=279, type=int, help='Maximum number of particles')
+    parser.add_argument('--data_path', default='/pscratch/sd/d/dimathan/LHCO/Data', help='Path containing the training files')
+    parser.add_argument('--load', action='store_true', default=False,help='Load trained model')
+    parser.add_argument('--large', action='store_true', default=False,help='Train with a large model')
+
+    flags = parser.parse_args()
+    with open(flags.config, 'r') as stream:
+        config = yaml.safe_load(stream)
+      
+    
+    data_size, training_data, test_data = utils.DataLoader(flags.data_path,
+                                                           flags.file_name,
+                                                           flags.npart,
+                                                           n_events = config['n_events'],
+                                                           rank = hvd.rank(), 
+                                                           size= hvd.size(),
+                                                           batch_size = config['BATCH'],
+                                                           norm=config['NORM'])
+
+    model = GSGM(config=config,npart=flags.npart)
+ 
+    model_name = config['MODEL_NAME']
+    if flags.large:
+        model_name+='_large'
+    checkpoint_folder = '../checkpoints_{}/checkpoint'.format(model_name)
+        
+        
+    lr_schedule = tf.keras.experimental.CosineDecay(
+        initial_learning_rate=config['LR']*hvd.size(),
+        decay_steps=config['MAXEPOCH']*int(data_size/config['BATCH'])
+    )
+
+    #opt = tf.keras.optimizers.Adam(learning_rate=config['LR']*hvd.size())
+    opt = tf.keras.optimizers.Adamax(learning_rate=lr_schedule)
+    opt = hvd.DistributedOptimizer(
+        opt, average_aggregated_gradients=True)
+
+        
+    model.compile(            
+        optimizer=opt,
+        #run_eagerly=True,
+        experimental_run_tf_function=False,
+        weighted_metrics=[])
+
+    if flags.load:
+        model.load_weights(checkpoint_folder).expect_partial()
+
+    
+    callbacks = [
+        hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+        hvd.callbacks.MetricAverageCallback(),
+        EarlyStopping(patience=50,restore_best_weights=True),
+    ]
+
+        
+    if hvd.rank()==0:
+        checkpoint = ModelCheckpoint(checkpoint_folder,mode='auto',
+                                     save_best_only=True,
+                                     save_weights_only=True, 
+                                     save_freq='epoch',)  # Save after each epoch
+        callbacks.append(checkpoint)
+        
+    
+    history = model.fit(
+        training_data,
+        epochs=config['MAXEPOCH'],
+        callbacks=callbacks,
+        steps_per_epoch=int(data_size/config['BATCH']),
+        validation_data=test_data,
+        validation_steps=int(data_size*0.1/config['BATCH']),
+        verbose=2 if hvd.rank()==0 else 0,
+        #steps_per_epoch=1,
+    )
+
+    
